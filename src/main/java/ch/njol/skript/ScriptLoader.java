c2c5187c01c9b5f52b52b8634e3bcab77bde3e6c
TPGamesNL <pet.teun.03@gmail.com>
2022-01-29 19:12:58 +0100

    Update ScriptLoader

--- src/main/java/ch/njol/skript/ScriptLoader.java
+++ src/main/java/ch/njol/skript/ScriptLoader.java
@@ -490,7 +490,7 @@ public class ScriptLoader {
 				}
 				
 				if (scriptInfo.files == 0)
-					Skript.warning(m_no_scripts.toString());
+					SkriptLogger.log(new LogEntry(Level.WARNING, m_no_scripts.toString(), null));
 				if (Skript.logNormal() && scriptInfo.files > 0)
 					Skript.info(m_scripts_loaded.toString(
 						scriptInfo.files,
